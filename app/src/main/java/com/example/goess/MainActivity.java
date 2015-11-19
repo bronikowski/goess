592a15dfceebd701c006f98de522340de275d1ff
dianull <diana.blaszczyk@gmail.com>
2015-11-19 21:04:01 +0100

    improved stone placing

--- app/src/main/java/com/example/goess/MainActivity.java
+++ app/src/main/java/com/example/goess/MainActivity.java
@@ -23,22 +23,28 @@ public class MainActivity extends AppCompatActivity {
 
     private static String TAG = "MainActivity";
     private static int GRID_PADDING = 20;
-    private static int STONE_SIZE = 22;
+    private static int STONE_SIZE = 28;
 
     ImageView boardImage;
+    ImageView stoneImage;
     Context context;
     int boardWidth;
     int boardHeight;
+    int stoneWidth;
+    int stoneHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar myToolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.my_toolbar);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
         context = this.getApplicationContext();
         boardImage = (ImageView) findViewById(R.id.backgroundImg);
+        stoneImage = (ImageView) findViewById(R.id.blackImg);
+
+        BoardLogic boardLogic = new BoardLogic();
 
 
         ViewTreeObserver vto = boardImage.getViewTreeObserver();
@@ -47,6 +53,12 @@ public class MainActivity extends AppCompatActivity {
                 boardImage.getViewTreeObserver().removeOnPreDrawListener(this);
                 boardWidth = boardImage.getMeasuredHeight();
                 boardHeight = boardImage.getMeasuredWidth();
+
+                stoneWidth = stoneImage.getMeasuredHeight();
+                stoneHeight = stoneImage.getMeasuredWidth();
+
+                Log.i(TAG, "boardWidth: " + String.valueOf(stoneWidth + ", boardHeight: " +
+                String.valueOf(stoneHeight)));
 
                 drawBoardGrid();
 
@@ -57,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
 
         final FrameLayout frameLayout = (FrameLayout)findViewById(R.id.background);
 
-        final ImageView stoneImg = (ImageView) findViewById(R.id.blackImg);
+
         final View horiz = (View) findViewById(R.id.horiz);
         final View vertic = (View) findViewById(R.id.vertic);
         horiz.setVisibility(View.INVISIBLE);
@@ -70,7 +82,7 @@ public class MainActivity extends AppCompatActivity {
                 FrameLayout.LayoutParams horizParam = new FrameLayout.LayoutParams(1, boardWidth);
                 FrameLayout.LayoutParams verticParam = new FrameLayout.LayoutParams(boardHeight, 1);
                 FrameLayout.LayoutParams stoneParam = new FrameLayout.LayoutParams(STONE_SIZE, STONE_SIZE);
-                ViewGroup owner = (ViewGroup) stoneImg.getParent();
+                ViewGroup owner = (ViewGroup) stoneImage.getParent();
 
                 int action = event.getAction();
                 switch (action) {
@@ -100,9 +112,26 @@ public class MainActivity extends AppCompatActivity {
                         ImageView img = new ImageView(context);
                         img.setImageDrawable(v.getResources().getDrawable(R.drawable.black));
 
-                        stoneParam.leftMargin = (int) event.getX();
-                        stoneParam.topMargin = (int) event.getY();
-                        frameLayout.addView(img, stoneParam);
+                        int eventX = (int) event.getX();
+                        int eventY = (int) event.getY();
+                        if (eventX > 15 && eventX < (boardWidth - 10)
+                            && eventY > 15 && eventY < (boardHeight - 10)) {
+
+                            float offsetW = (boardWidth - (GRID_PADDING * 2)) / 18;
+                            float offsetH = (boardHeight - (GRID_PADDING * 2)) / 18;
+                            int x = ((int) (eventX / offsetW)) + 1;
+                            int y = ((int) (eventY / offsetH)) + 1;
+
+                            if (x > 19)
+                                x = 19;
+                            if (y > 19)
+                                y = 19;
+
+                            stoneParam.leftMargin = ((int) (x * offsetW)) - (STONE_SIZE / 2) + 1/*imgpadding*/;
+                            stoneParam.topMargin = ((int) (y * offsetH)) - (STONE_SIZE / 2);
+                            frameLayout.addView(img, stoneParam);
+
+                        }
 
                         break;
                     case MotionEvent.ACTION_CANCEL:
