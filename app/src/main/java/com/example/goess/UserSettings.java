4603a87d6962e87dc13d39dbc6fb36ea0d748f0d
dianull <diana.blaszczyk@gmail.com>
2015-12-26 17:18:35 +0100

    saving user settings

---
+++ app/src/main/java/com/example/goess/UserSettings.java
@@ -0,0 +1,44 @@
+package com.example.goess;
+
+
+import android.content.SharedPreferences;
+import android.util.Log;
+
+public class UserSettings {
+
+    enum Metrics {
+        EUCLID,
+        STANDARD
+    }
+
+    boolean showBoardCoords;
+    boolean showIndicator;
+    Metrics metrics;
+    SharedPreferences preferences;
+
+    UserSettings(SharedPreferences preferences) {
+        this.preferences = preferences;
+        if (preferences != null) {
+            showBoardCoords = preferences.getBoolean("showBoardCoords", false);
+            showIndicator = preferences.getBoolean("showIndicator", true);
+            metrics = preferences.getBoolean("euclidean", false) ? Metrics.EUCLID : Metrics.STANDARD;
+        } else
+            setIndicator(true);
+    }
+
+    public void setShowBoardCoords(boolean show) {
+        showBoardCoords = show;
+        preferences.edit().putBoolean("showBoardCoords", show).apply();
+    }
+
+    public void setIndicator(boolean show) {
+        showIndicator = show;
+        preferences.edit().putBoolean("showIndicator", show).apply();
+    }
+
+    public void setMetrics(boolean euclid) {
+        this.metrics = euclid ? Metrics.EUCLID : Metrics.STANDARD;
+        preferences.edit().putBoolean("euclidean", euclid).apply();
+    }
+
+}
